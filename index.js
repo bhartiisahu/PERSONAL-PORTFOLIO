2306d198777326f1284942d14abaeae14e345b47
Bharti Sahu <sahu93025369@gmail.com>
2024-08-04 21:14:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import initScrollReveal from "scrollReveal";
-import initTiltEffect from "tiltAnimation";
-import { targetElements, defaultProps } from "scrollRevealConfig";
+./import initScrollReveal from "scrollReveal";
+import initTiltEffect from "./tiltAnimation";
+import { targetElements, defaultProps } from "./scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
